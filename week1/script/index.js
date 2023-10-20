209132eef78cc28e37ea9b6cfd8d14e1047d4278
Fachminur1 <fachminur2836@gmail.com>
2023-10-20 20:15:27 +0700

    changing and organizing files

---
+++ week1/script/index.js
@@ -0,0 +1,15 @@
+function button1() {
+    window.location.href = 'qubyhappy.html';
+}
+
+function button2() {
+    window.location.href = 'qubyworker.html';
+}
+
+function button3() {
+    window.location.href = 'qubyfood.html';
+}
+
+function button4() {
+    window.location.href = 'qubylazy.html';
+}
